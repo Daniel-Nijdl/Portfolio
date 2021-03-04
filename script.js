3731dfa2d77bdfe726472222a9f5c630c0ffcb72
Daniel Nijdl <Dnijdl267@west-mec.local>
2021-03-04 15:26:24 -0700

    Added some stuff and got form working

---
+++ script.js
@@ -0,0 +1,24 @@
+// class User {
+//     constructor(yName, yEmail, ySubject, yMessage){
+//         this.yName = yName;
+//         this.yEmail = yEmail;
+//         this.ySubject = ySubject;
+//         this.yMessage = yMessage;
+//     }
+// }
+
+// function addToList(){
+//     let yName = document.getElementsByClassName("yName").value;
+//     let yEmail = document.getElementsByClassName("yEmail").value;
+//     let ySubject = document.getElementsByClassName("ySubject").value;
+//     let yMessage = document.getElementsByClassName("yMessage").value;
+// }
+
+
+// let info = "";
+
+// $(function(){
+    $(`.subButton `).on(`click`, function(){
+        document.forms['contact-form'].reset()
+    })
+// })
